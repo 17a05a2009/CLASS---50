674f531e5700108a0ccac026ef92b3a9351616e4
17a05a2009 <107633223+17a05a2009@users.noreply.github.com>
2022-11-21 19:50:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var roboat ;
+var ground, invisibleGround, groundImage;
+
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
+var score;
+var gameOverImg,restartImg
